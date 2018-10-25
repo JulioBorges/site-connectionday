ac8352aae449bc7f2c1440eaac2b766d5fed8eca
JULIOBORGES <borges.juliocezar@gmail.com>
2018-10-25 16:17:42 -0300

    Finalização da criação de componentes

---
+++ src/app/subfooter/subfooter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-subfooter',
+  templateUrl: './subfooter.component.html',
+  styleUrls: ['./subfooter.component.css']
+})
+export class SubfooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
