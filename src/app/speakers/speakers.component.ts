e13b385c3f004ace642dec0c54a3d80ff155c953
JulioBorges <borges.juliocezar@gmail.com>
2019-03-13 15:05:45 -0300

    Inclusão de palestrantes

--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import * as Speakers from '../../assets/config/speakers.json';
+import { Globals } from '../../globals';
 
 @Component({
   selector: 'app-speakers',
   templateUrl: './speakers.component.html',
-  styleUrls: ['./speakers.component.css']
+  styleUrls: ['./speakers.component.css'],
+  providers: [ Globals ]
 })
 export class SpeakersComponent implements OnInit {
+  protected vm: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public globals: Globals) { 
   }
 
+  ngOnInit() {
+    this.vm = Speakers.default;
+  }
+
+  clickPopup(speaker: any) {
+    this.globals.SPEAKER = speaker;
+  }
 }
